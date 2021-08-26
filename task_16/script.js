098c3dd0a2d4aecc760c4f0b27f9484227635ce7
hayk inants <hayk@phoenixcs.ru>
2021-08-26 13:51:21 +0300

    add 16 task

---
+++ task_16/script.js
@@ -0,0 +1,86 @@
+const regexpStyle = /^[^{@]+{[^}]+}/gm;
+const clearRegexp = /^\@[a-z {}\n0-9%:\-\;]+}\n}$/gm;
+const html = document.querySelector('#html');
+const css  = document.querySelector('#css');
+let btn  = document.querySelector('#submit');
+
+
+html.value = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <img src="https://pngicon.ru/file/uploads/Osenniy-list-klena.png" alt="">
+</body>
+</html>
+`;
+
+css.value = `
+body {
+  background-color: #6f9e80;
+  margin: 0;
+  font-family: sans-serif;
+  padding: 20px;
+}
+
+@keyframes example {
+    0% {left: -100px}
+    50% {left: 60vw; top: 100px}
+    60% {left: 30vw; top: 100px}
+    100% {left: 100vw; top: 200px}
+}
+
+
+img{
+  width: 100px;
+  position: absolute;
+  left: -100px;
+  top: 200px;
+}
+
+`
+
+const cssHandler = (cssText, dom)=>{
+	console.log(cssText.replace(clearRegexp, ''));
+	[...cssText.replace(clearRegexp, '').match(regexpStyle)].map(style=>{
+		const selector = style.match(/^([^{@]+){/g)[0].replaceAll(/[\n{]/g, '');
+		const styleProps = style.match(/{[^}]+}/g)[0].replaceAll(/[\n{}]/g, '');
+		return [selector, styleProps]
+	}).forEach(style=>{
+		[...dom.querySelectorAll(style[0])].forEach(elem=>elem.style = elem.style.cssText+style[1]);
+	})
+
+}
+
+const inputHandler = ()=>{
+	const result = document.createElement('iframe');
+	document.querySelector('#forResult').innerText = '';
+	document.querySelector('#forResult').appendChild(result);
+	const innerDom = result.contentDocument;
+
+	const htmlValue = html.value;
+	const cssValue = css.value;
+	const styleTag = document.createElement('style');
+
+	styleTag.type = 'text/css';
+	if (styleTag.styleSheet){
+	  styleTag.styleSheet.cssText = cssValue;
+	} else {
+	  styleTag.appendChild(document.createTextNode(cssValue));
+	}
+	innerDom.write('');
+	innerDom.write(htmlValue);
+	try{
+		cssHandler(css.value, innerDom);
+	}catch{}
+	innerDom.querySelector('head').appendChild(styleTag);
+};
+
+
+html.addEventListener('input', inputHandler);
+css.addEventListener('input', inputHandler);
---
+++ task_16/tests.js
@@ -0,0 +1,89 @@
+
+
+const tests = [
+
+	{
+		"msg": "Укажите для тега img имя анимации",
+		"tag": 'img',
+		"class": undefined,
+		"id": undefined,
+		'styleList':{
+			'animation-name': 'example'
+		}
+	},
+	{
+		"msg": "Укажите для тега img имя продолжительность анимации в 3 секунды",
+		"tag": 'img',
+		"class": undefined,
+		"id": undefined,
+		'styleList':{
+			'animation-duration': '3s'
+		}
+	},
+	{
+		"msg": "Укажите для тега img колличество повторений анимации 2 раза",
+		"tag": 'img',
+		"class": undefined,
+		"id": undefined,
+		'styleList':{
+			'animation-iteration-count': '2'
+		}
+	},
+	{
+		"msg": "Укажите для тега img, что тот должен применяться до и после анимации тот стиль, который соответствует концу анимации",
+		"tag": 'img',
+		"class": undefined,
+		"id": undefined,
+		'styleList':{
+			'animation-fill-mode': 'forwards'
+		}
+	},
+	{
+		"msg": "Укажите для тега img функцию анимации linear",
+		"tag": 'img',
+		"class": undefined,
+		"id": undefined,
+		'styleList':{
+			'animation-timing-function': 'linear'
+		}
+	}
+]
+
+const testHandler = ()=>{
+	const dom = document.querySelector('iframe').contentDocument;
+	document.querySelector('#testResult').innerText = '';
+
+	tests.forEach(elem=>{
+		const answer = document.createElement('li');
+
+		let selector = ''
+		if (elem.tag) selector	 += elem.tag;
+		if (elem.id) selector	 += `#${elem.id}`;
+		if (elem.class) selector += `.${elem.class}`;
+		let testResult = true;
+		if (dom.querySelectorAll(selector).length === 0){
+			testResult = false;	
+		}else{
+			dom.querySelectorAll(selector).forEach(selectorElem=>{
+				if (elem.innerText){
+					testResult &&= (selectorElem.innerText.trim() === elem.innerText);
+				}
+				try{
+					Object.entries(elem.styleList).forEach(style=>{
+					testResult &&= (selectorElem.style[style[0]] === style[1]);
+				})} catch{};
+			})
+		}
+		if (testResult){
+			answer.classList.add('pos');
+		}else{
+			answer.classList.add('neg');
+		}
+
+
+		answer.innerText = elem.msg;
+		document.querySelector('#testResult').appendChild(answer);
+	})
+}
+
+btn.addEventListener('click', testHandler);
