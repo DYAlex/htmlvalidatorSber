bf49545eeb7a480d59743cb24d3c23d084b99f8b
hayk inants <hayk@phoenixcs.ru>
2021-08-28 14:01:59 +0300

    add 28 task

---
+++ task_28/script.js
@@ -0,0 +1,95 @@
+const regexpStyle = /^[^{@]+{[^}]+}/gm;
+const clearRegexp = /^\@[a-z {}\n0-9%:\-\;]+}\n}$/gm;
+const html = document.querySelector('#html');
+const css  = document.querySelector('#css');
+let btn  = document.querySelector('#submit');
+
+
+
+html.value = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title> 
+</head>
+<body>
+<form oninput="city_out.value=city.value;">
+ <label>Выберите город доставки: </label>
+   <div class='fields'>
+   </div>
+  <p>Результат: <output name='city_out' for='city'></output></p>
+ <form>
+
+</body>
+</html>
+
+
+`;
+
+css.value = `
+
+body {
+  background-color: #6f9e80;
+  margin: 0;
+  font-family: sans-serif;
+  margin: 10px;
+} 
+
+select{
+    margin-top: 20px;
+    display: block;
+}
+
+.fields{
+	margin-top: 10px;
+}
+
+p{
+	display: inline-block;
+}
+
+
+`
+
+const cssHandler = (cssText, dom)=>{
+	console.log(cssText.replace(clearRegexp, ''));
+	[...cssText.replace(clearRegexp, '').match(regexpStyle)].map(style=>{
+		const selector = style.match(/^([^{@]+){/g)[0].replaceAll(/[\n{]/g, '');
+		const styleProps = style.match(/{[^}]+}/g)[0].replaceAll(/[\n{}]/g, '');
+		return [selector, styleProps]
+	}).forEach(style=>{
+		[...dom.querySelectorAll(style[0])].forEach(elem=>elem.style = elem.style.cssText+style[1]);
+	})
+
+}
+
+const inputHandler = ()=>{
+	const result = document.createElement('iframe');
+	document.querySelector('#forResult').innerText = '';
+	document.querySelector('#forResult').appendChild(result);
+	const innerDom = result.contentDocument;
+
+	const htmlValue = html.value;
+	const cssValue = css.value;
+	const styleTag = document.createElement('style');
+
+	styleTag.type = 'text/css';
+	if (styleTag.styleSheet){
+	  styleTag.styleSheet.cssText = cssValue;
+	} else {
+	  styleTag.appendChild(document.createTextNode(cssValue));
+	}
+	innerDom.write('');
+	innerDom.write(htmlValue);
+	try{
+		cssHandler(css.value, innerDom);
+	}catch{}
+	innerDom.querySelector('head').appendChild(styleTag);
+};
+
+
+html.addEventListener('input', inputHandler);
+css.addEventListener('input', inputHandler);
---
+++ task_28/tests.js
@@ -0,0 +1,86 @@
+
+
+const tests = [
+	{
+		"msg": 'Добавьте в тег div с классом fields два тега input с атрибутом name = "city"',
+		"tag": 'div input[name="city"]:nth-child(2)',
+		"class": undefined,
+		"id": undefined
+	},
+	{
+		"msg": 'Укажите тегам input атрибут type со значением radio',
+		"tag": 'input[type="radio"]',
+		"class": undefined,
+		"id": undefined
+	},
+	{
+		"msg": 'Добавьте тег p со значением "Москва" перед первым тегом input',
+		"tag": 'p:nth-child(1)',
+		"class": undefined,
+		"id": undefined,
+		'innerText': 'Москва'
+	},
+	{
+		"msg": 'Добавьте тег p со значением "Питер" перед вторым тегом input',
+		"tag": 'input+p',
+		"class": undefined,
+		"id": undefined,
+		"innerText": "Питер"
+	},
+	{
+		"msg": 'Укажите первому тегу input атрибут value со значением msc, а так же атрибут checked',
+		"tag": 'input:nth-child(2)[value="msc"][checked]',
+		"class": undefined,
+		"id": undefined
+	},
+		{
+		"msg": 'Укажите второму тегу input атрибут value со значением spb',
+		"tag": 'input:nth-child(4)[value="spb"]',
+		"class": undefined,
+		"id": undefined
+	}
+
+]
+
+const testHandler = ()=>{
+	const dom = document.querySelector('iframe').contentDocument;
+	document.querySelector('#testResult').innerText = '';
+
+	tests.forEach(elem=>{
+		const answer = document.createElement('li');
+
+		let selector = ''
+		if (elem.tag) selector	 += elem.tag;
+		if (elem.id) selector	 += `#${elem.id}`;
+		if (elem.class) selector += `.${elem.class}`;
+		let testResult = true;
+		if (dom.querySelectorAll(selector).length === 0){
+			testResult = false;	
+		}else{
+			dom.querySelectorAll(selector).forEach(selectorElem=>{
+				if (elem.innerText){
+					testResult &&= (selectorElem.innerText.trim() === elem.innerText);
+					console.log('_-_-')
+					console.log(selectorElem.innerText.trim())
+					console.log(elem.innerText)
+					console.log('_-_-')
+				}
+				try{
+					Object.entries(elem.styleList).forEach(style=>{
+					testResult &&= (selectorElem.style[style[0]] === style[1]);
+				})} catch{};
+			})
+		}
+		if (testResult){
+			answer.classList.add('pos');
+		}else{
+			answer.classList.add('neg');
+		}
+
+
+		answer.innerText = elem.msg;
+		document.querySelector('#testResult').appendChild(answer);
+	})
+}
+
+btn.addEventListener('click', testHandler);
